dd42090ad41b3e763554cad1933a99f7de3fcf2d
vertrauterdavid <vertrauterdavid@gmail.com>
2024-04-11 19:22:41 +0200

    Fixed sql & updated version

--- src/main/java/net/vertrauterdavid/homes/listener/PlayerJoinListener.java
+++ src/main/java/net/vertrauterdavid/homes/listener/PlayerJoinListener.java
@@ -1,6 +1,7 @@
 package net.vertrauterdavid.homes.listener;
 
 import net.vertrauterdavid.homes.Homes;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,6 +16,8 @@ public class PlayerJoinListener implements Listener {
         if (!(Homes.getInstance().getSqlUtil().get("Homes", "UUID", "UUID='" + player.getUniqueId().toString() + "'").equalsIgnoreCase(player.getUniqueId().toString()))) {
             Homes.getInstance().getSqlUtil().update("INSERT INTO Homes (UUID) VALUES ('" + player.getUniqueId().toString() + "')");
         }
+
+        Bukkit.getScheduler().runTaskAsynchronously(Homes.getInstance(), () -> Homes.getInstance().getHomeUtil().loadLocal(player.getUniqueId()));
     }
 
 }
--- src/main/java/net/vertrauterdavid/homes/util/HomeUtil.java
+++ src/main/java/net/vertrauterdavid/homes/util/HomeUtil.java
@@ -1,28 +1,59 @@
 package net.vertrauterdavid.homes.util;
 
+import lombok.Getter;
 import net.vertrauterdavid.homes.Homes;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
+import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.UUID;
 
+@Getter
 public class HomeUtil {
 
+    private final HashMap<String, String> localStorage = new HashMap<>();
+
+    public void loadLocal(UUID uuid) {
+        ResultSet resultSet = Homes.getInstance().getSqlUtil().getResult("SELECT * FROM Homes WHERE UUID='" + uuid.toString() + "'");
+        try {
+            while (resultSet.next()) {
+                localStorage.put(uuid + "-1", resultSet.getString("Home1"));
+                localStorage.put(uuid + "-2", resultSet.getString("Home2"));
+                localStorage.put(uuid + "-3", resultSet.getString("Home3"));
+                localStorage.put(uuid + "-4", resultSet.getString("Home4"));
+                localStorage.put(uuid + "-5", resultSet.getString("Home5"));
+                localStorage.put(uuid + "-6", resultSet.getString("Home6"));
+                localStorage.put(uuid + "-7", resultSet.getString("Home7"));
+            }
+        } catch (Exception ignored) { }
+    }
+
     public void set(UUID uuid, int number, Location location) {
-        Homes.getInstance().getSqlUtil().update("UPDATE Homes SET Home" + number + "='" + location.getWorld().getName() + "/" + (location.getBlockX() + 0.5) + "/" + location.getBlockY() + "/" + (location.getBlockZ() + 0.5) + "/" + (Math.round(location.getYaw() / 45) * 45) + "' WHERE UUID='" + uuid.toString() + "'");
+        String s = location.getWorld().getName() + "/" + (location.getBlockX() + 0.5) + "/" + location.getBlockY() + "/" + (location.getBlockZ() + 0.5) + "/" + (Math.round(location.getYaw() / 45) * 45);
+        Homes.getInstance().getSqlUtil().update("UPDATE Homes SET Home" + number + "='" + s + "' WHERE UUID='" + uuid.toString() + "'");
+        localStorage.put(uuid + "-" + number, s);
     }
 
     public void delete(UUID uuid, int number) {
         Homes.getInstance().getSqlUtil().update("UPDATE Homes SET Home" + number + "='-' WHERE UUID='" + uuid.toString() + "'");
+        localStorage.put(uuid + "-" + number, "-");
     }
 
     public Location get(UUID uuid, int number) {
-        String config = Homes.getInstance().getSqlUtil().get("Homes", "Home" + number, "UUID='" + uuid.toString() + "'");
-        if (config.equalsIgnoreCase("-")) {
+        String s;
+        if (localStorage.containsKey(uuid + "-" + number)) {
+            s = localStorage.get(uuid + "-" + number);
+        } else {
+            s = Homes.getInstance().getSqlUtil().get("Homes", "Home" + number, "UUID='" + uuid.toString() + "'");
+            localStorage.put(uuid + "-" + number, s);
+        }
+
+        if (s.equalsIgnoreCase("-")) {
             return null;
         }
 
-        String[] locationData = config.split("/");
+        String[] locationData = s.split("/");
         return new Location(
                 Bukkit.getWorld(locationData[0]),
                 Double.parseDouble(locationData[1]),
